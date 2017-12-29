a38fafe345422f4c3a9df0b6ea86154cdeae830e
nikhilkesari <nikhil.kesari@bigbasket.com>
2017-12-29 12:19:54 +0530

    added css loaders

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
 angular.module('checkout', []);
 
 require('./checkout/shell');
+require('./assets/style.css');
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
 const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 
 module.exports = {
-    context: __dirname + '/app',
     entry: {
-        app: './app.js',
+        app: './app/app.js',
         vendor: ['angular']
     },
     devServer: {
         contentBase: './dist'
     },
     output: {
-        path: __dirname + '/dist',
+        path: path.resolve(__dirname, 'dist'),
         filename: 'app.bundle.js'
     },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            })
+        }]
+    },
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin({ "name": "vendor", "filename": "vendor.bundle.js" })
+        new webpack.optimize.CommonsChunkPlugin({ "name": "vendor", "filename": "vendor.bundle.js" }),
+        new ExtractTextPlugin("styles.css")
     ]
 };
